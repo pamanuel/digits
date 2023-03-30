c474c0cb35ff427e93019c3e19979f9c3b57345c
pmmanuel315 <pmmanuel315@gmail.com>
2023-03-29 18:35:54 -1000

    37:45

--- app/imports/ui/pages/ListContactAdmin.jsx
+++ app/imports/ui/pages/ListContactAdmin.jsx
@@ -3,16 +3,16 @@ import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
-import Contact from '../components/Contact';
+import ContactAdmin from '../components/ContactAdmin';
 import { Contacts } from '../../api/contact/Contacts';
 /* Renders a table containing all the Stuff documents. Use <StuffItem> to render each row. */
-const ListContact = () => {
+const ListContactAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe(Contacts.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -28,10 +28,10 @@ const ListContact = () => {
       <Row className="justify-content-center">
         <Col md={7}>
           <Col className="text-center">
-            <h2>List Contacts</h2>
+            <h2>List Contacts (Admin)</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {contacts.map((contact) => <Col key={contact._id}><Contact contact={contact} /></Col>)}
+            {contacts.map((contact) => <Col key={contact._id}><ContactAdmin contact={contact} /></Col>)}
           </Row>
         </Col>
       </Row>
@@ -39,4 +39,4 @@ const ListContact = () => {
   ) : <LoadingSpinner />);
 };
 
-export default ListContact;
+export default ListContactAdmin;
